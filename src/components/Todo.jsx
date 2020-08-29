7fec23d398313ac4145fa32368c5e579c3d08916
myzeus <59224888+myzeus@users.noreply.github.com>
2020-08-29 21:29:40 +0700

    Complete Component header, todo, todoform using component

---
+++ src/components/Todo.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Todo = () => {
+  return (
+    <section className="todos">
+      <div className="todo">
+        <span className="todo-text">Learning react</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning react</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning react</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning react</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning react</span>
+      </div>
+      <div className="todo">
+        <span className="todo-text">Learning react</span>
+      </div>
+    </section>
+  );
+};
+
+export default Todo;
